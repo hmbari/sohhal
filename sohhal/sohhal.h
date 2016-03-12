06e5600fa03154e8d7b2bf31dc891c70670c6870
Jakir H M Andulawal <jakirhmandulawal@jakirs-macbook-air.local>
2016-03-12 04:56:53 +0300

    Initial Commit

---
+++ sohhal/sohhal.h
@@ -0,0 +1,19 @@
+//
+//  sohhal.h
+//  sohhal
+//
+//  Created by Jakir H M Andulawal on 3/12/16.
+//  Copyright © 2016 Jakir H M Andulawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sohhal.
+FOUNDATION_EXPORT double sohhalVersionNumber;
+
+//! Project version string for sohhal.
+FOUNDATION_EXPORT const unsigned char sohhalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sohhal/PublicHeader.h>
+
+
